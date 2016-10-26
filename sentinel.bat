51128163471f4c55673980c63fd41b3d0ba77730
Stas Ukolov <ukoloff@gmail.com>
2016-10-26 17:13:17 +0500

    Start XML templating

---
+++ sentinel.bat
@@ -0,0 +1,107 @@
+0</*! ::
+@echo off
+cscript //nologo //e:javascript "%~f0" %*
+goto :EOF */0;
+
+//     Underscore.js 1.8.3
+//     http://underscorejs.org
+
+templateSettings = {
+  variable    : '$',
+  evaluate    : /<%([\s\S]+?)%>/g,
+  interpolate : /<%=([\s\S]+?)%>/g,
+  escape      : /<%-([\s\S]+?)%>/g
+};
+
+var escapes = {
+  "'":      "'",
+  '\\':     '\\',
+  '\r':     'r',
+  '\n':     'n',
+  '\u2028': 'u2028',
+  '\u2029': 'u2029'
+};
+
+var escaper = /\\|'|\r|\n|\u2028|\u2029/g;
+
+var escapeChar = function(match) {
+  return '\\' + escapes[match];
+};
+
+// JavaScript micro-templating, similar to John Resig's implementation.
+// Underscore templating handles arbitrary delimiters, preserves whitespace,
+// and correctly escapes quotes within interpolated code.
+// NB: `oldSettings` only exists for backwards compatibility.
+function template(text) {
+  var settings = templateSettings;
+
+  // Combine delimiters into one regular expression via alternation.
+  var matcher = RegExp([
+    (settings.escape || noMatch).source,
+    (settings.interpolate || noMatch).source,
+    (settings.evaluate || noMatch).source
+  ].join('|') + '|$', 'g');
+
+  // Compile the template source, escaping string literals appropriately.
+  var index = 0;
+  var source = "__p+='";
+  text.replace(matcher, function(match, escape, interpolate, evaluate, offset) {
+    source += text.slice(index, offset).replace(escaper, escapeChar);
+    index = offset + match.length;
+
+    if (escape) {
+      source += "'+\n((__t=(" + escape + "))==null?'':_.escape(__t))+\n'";
+    } else if (interpolate) {
+      source += "'+\n((__t=(" + interpolate + "))==null?'':__t)+\n'";
+    } else if (evaluate) {
+      source += "';\n" + evaluate + "\n__p+='";
+    }
+
+    // Adobe VMs need the match returned to produce the correct offest.
+    return match;
+  });
+  source += "';\n";
+
+  // If a variable is not specified, place data values in local scope.
+  if (!settings.variable) source = 'with(obj||{}){\n' + source + '}\n';
+
+  source = "var __t,__p='',__j=Array.prototype.join," +
+    "print=function(){__p+=__j.call(arguments,'');};\n" +
+    source + 'return __p;\n';
+
+  try {
+    var render = new Function(settings.variable || 'obj', '_', source);
+  } catch (e) {
+    e.source = source;
+    throw e;
+  }
+
+  var template = function(data) {
+    return render.call(this, data);
+  };
+
+  // Provide the compiled source as a convenience for precompilation.
+  var argument = settings.variable || 'obj';
+  template.source = 'function(' + argument + '){\n' + source + '}';
+
+  return template;
+};
+
+// Array::forEach polyfill
+
+if(!Array.prototype.forEach)
+Array.prototype.forEach = function(fn) {
+  for (var i = 0, len = this.length; i < len; i++)
+    fn(this[i], i, this)
+}
+
+function readFile(name)
+{
+  var
+    fso = new ActiveXObject("Scripting.FileSystemObject")
+    name = fso.GetParentFolderName(WScript.ScriptFullName)+"/"+name
+    return fso.OpenTextFile(name).ReadAll()
+}
+
+var t = template(readFile('Sirius!.prjx'))
+WScript.Echo(t.source)
